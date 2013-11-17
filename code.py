c87dc3805d2e28d00e63070b9a1ddb6a8c1c6f9c
Scott Trocchia <scotttrocchia@gmail.com>
2013-11-16 23:31:10 -0500

    uploaded new app as myfood2p0.appspot.com. Still need to get bootstrap templates working properly...

--- code.py
+++ code.py
@@ -10,7 +10,6 @@ from collections import defaultdict
 urls = (
 	"/", "index",
 	"/getcsv", "getcsv"
-	#"/userI", "userI"
 )
 
 render = web.template.render('templates/')
@@ -31,20 +30,7 @@ foodForm = form.Form(
 #myUserDict = web.session.Session(app, web.session.DiskStore('userdicts'))
 #user_store = web.session.Session(app, web.session.DiskStore('user_store'))
 
-#fromform = dict()
-#fromform['mealType'] = ''
-#fromform['chosen_month'] = ''
-
 double_dict = defaultdict(dict)
-userList = []
-CSVrows = []
-
-Ltot_thisMonth = 0.0
-Dtot_thisMonth = 0.0
-Mtot_thisMonth = 0.0
-Ltot_All = 0.0
-Dtot_All = 0.0
-Mtot_All = 0.0
 
 class index:
 
@@ -52,29 +38,19 @@ class index:
 		form = foodForm()
 		
 		if not form.validates():
-			return render.init_form(form)
+			return render.init_form(form, "")
 		else:
-			global userList, CSVrows, Ltot_thisMonth, Dtot_thisMonth, Mtot_thisMonth, Ltot_All, Dtot_All, Mtot_All
+			global user
+			clearMessage = ""
 
 			if form['Clear? (y/n)'].value == 'y':
-			#	Ltot_thisMonth = 0.0
-			#	Dtot_thisMonth = 0.0
-			#	Mtot_thisMonth = 0.0
-			#	Ltot_All = 0.0
-			#	Dtot_All = 0.0
-			#	Mtot_All = 0.0
-			#	fromform.clear()
-			#	userList = []
-			#	dateList = []
-			#	lunchList = []
-			#	dinnerList = []
-			#	miscList = []
-			#	CSVrows = []
-				return render.init_form(form)
-		
+				double_dict.clear()
+				clearMessage = "Your personal meal expense database is cleared!"
+				return render.init_form(form, clearMessage)
+	
 			else:
-				# saving convenient values to make it easier to reference them in future parts of the code
-				username = form['User name'].value
+				### saving convenient values to make it easier to reference them in future parts of the code
+				user = form['User name'].value
 				mealType= form['Which meal?'].value
 				chosen_month  = form['Month'].value
 				LDM = float(form['Lunch, Dinner, or Miscellaneous $'].value)
@@ -87,76 +63,75 @@ class index:
 
 				print 'DD b4: %s' % double_dict
 
-				if username not in double_dict:
-					# create the nested dict fields
-					double_dict[username]['dateList'] = []
-					double_dict[username]['lunchList'] = []
-					double_dict[username]['dinnerList'] = []
-					double_dict[username]['miscList'] = []
-					double_dict[username]['Ltot_All'] = ['0']
+				if not user in double_dict:
+					### create the nested dict fields
+					double_dict[user]['dateList'] = []
+					double_dict[user]['lunchList'] = []
+					double_dict[user]['dinnerList'] = []
+					double_dict[user]['miscList'] = []
+					double_dict[user]['CSVrows'] = []
+					double_dict[user]['Ltot_thisMonth'] = 0.00
+					double_dict[user]['Dtot_thisMonth'] = 0.00
+					double_dict[user]['Mtot_thisMonth'] = 0.00
+					double_dict[user]['Ltot_All'] = 0.00
+					double_dict[user]['Dtot_All'] = 0.00
+					double_dict[user]['Mtot_All'] = 0.00
 				else:
 					print "User already exists!"
 				
-				# add to those fields
-				if not joined_date in double_dict[username]['dateList']:
-					double_dict[username]['dateList'].append(joined_date)
+				### add to those fields
+				if not joined_date in double_dict[user]['dateList']:
+					double_dict[user]['dateList'].append(joined_date)
+				
 				if mealType == "Lunch":
-					double_dict[username]['lunchList'].append(LDM)
-					last_L_entry = float(double_dict[username]['Ltot_All'][-1])
-					#print last_L_entry
-					double_dict[username]['Ltot_All'].append(str(last_L_entry + LDM))
+					double_dict[user]['lunchList'].append(LDM)
+					double_dict[user]['Ltot_All'] += LDM
+					if chosen_month == currentMonth:
+						double_dict[user]['Ltot_thisMonth'] += LDM
 				elif mealType == "Dinner":
-					double_dict[username]['dinnerList'].append(LDM)
+					double_dict[user]['dinnerList'].append(LDM)
+					double_dict[user]['Dtot_All'] += LDM
+					if chosen_month == currentMonth:
+						double_dict[user]['Dtot_thisMonth'] += LDM
 				else:
-					double_dict[username]['miscList'].append(LDM)
-
+					double_dict[user]['miscList'].append(LDM)
+					double_dict[user]['Mtot_All'] += LDM
+					if chosen_month == currentMonth:
+						double_dict[user]['Mtot_thisMonth'] += LDM
+				
 				print 'DD after: %s' % double_dict
 
-				#myUserDict['username'] = form['User name'].value	
-				#if not myUserDict['username'] in userList:
-				#	userList.append(myUserDict['username'])
-
-				#if mealType == 'Lunch':
-					#lunchList.append(LDM)
-				#	Ltot_All += LDM
-				#	if chosen_month == currentMonth:
-				#		Ltot_thisMonth += LDM
-				#elif mealType == 'Dinner':
-				#	#dinnerList.append(LDM)
-				#	Dtot_All += LDM
-				#	if chosen_month == currentMonth:
-				#		Dtot_thisMonth += LDM
-				#else:
-				#	#miscList.append(LDM)
-				#	Mtot_All += LDM
-				#	if chosen_month == currentMonth:
-				#		Mtot_thisMonth += LDM
-
-				#CSVrows = map(None, dateList, lunchList, dinnerList, miscList)
+				double_dict[user]['CSVrows'] = map(None, double_dict[user]['dateList'], double_dict[user]['lunchList'], double_dict[user]['dinnerList'], double_dict[user]['miscList'])
 	
-				#return render.returned_data(CSVrows, currentMonth, Ltot_thisMonth, Dtot_thisMonth, Mtot_thisMonth, Ltot_All, Dtot_All, Mtot_All)
+				print 'CSVrows: %s' % double_dict[user]['CSVrows']
+				
+				return render.returned_data(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
+				#return render.returned_data_bootstrap(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
 
 	def GET(self):
 		form = foodForm()
-		return render.init_form(form)
+		return render.init_form(form, "")
 
 
 """ Render and generate a CSV file, which includes all user input information up to this point in time """
 class getcsv:
 
 	def GET(self):
-	
-		global CSVrows
 
+		global user
+		if user:
+			print "personalized CSVrows: %s" % double_dict[user]['CSVrows']
+
+		#if double_dict[user]['CSVrows']:
 		csv_file = StringIO()
 		csv_writer = csv.writer(csv_file)
 		csv_writer.writerow(['Date', 'Lunch', 'Dinner', 'Miscellaneous'])
-		#for i in CSVrows:
-		#	csv_writer.writerow(i)
+		for i in double_dict[user]['CSVrows']:
+			csv_writer.writerow(i)
 
 		currentDate = datetime.datetime.today().strftime('%m-%d-%Y')
 		web.header('Content-Type', 'text/csv')
-		web.header('Content-disposition', 'attachment; filename=myFood_' + currentDate + '.csv')			
+		web.header('Content-disposition', 'attachment; filename=MYFOOD_' + user.upper() + '_' + currentDate + '.csv')			
 		return csv_file.getvalue()
 
 
--- templates/__init__.py
+++ templates/__init__.py
@@ -2,7 +2,7 @@ from web.template import CompiledTemplate, ForLoop, TemplateResult
 
 
 # coding: utf-8
-def init_form (form):
+def init_form (form, clearMessage):
     __lineoffset__ = -4
     loop = ForLoop()
     self = TemplateResult(); extend_ = self.extend
@@ -12,9 +12,12 @@ def init_form (form):
     extend_([u'<form name="main" method="post">\n'])
     extend_([escape_(form.render(), False), u'\n'])
     extend_([u'<input type="submit" />\n'])
+    if clearMessage:
+        extend_([u'    ', escape_(clearMessage, True), u'\n'])
     extend_([u'</form>\n'])
     extend_([u'\n'])
-    extend_([u'Go to <a href="/getcsv">myfood-2p0.appspot.com/getcsv</a> to retrieve a compiled CSV file of your meal expenses.\n'])
+    extend_([u'<br></br>\n'])
+    extend_([u'After entering data, go to <a href="/getcsv">myfood2p0.appspot.com/getcsv</a> to retrieve a personalized CSV file of your meal expenses.\n'])
     extend_([u'\n'])
     extend_([u'<br></br>\n'])
     extend_([u'<br></br>\n'])
@@ -26,7 +29,7 @@ init_form = CompiledTemplate(init_form, 'templates/init_form.html')
 join_ = init_form._join; escape_ = init_form._escape
 
 # coding: utf-8
-def returned_data (CSVrows, currentMonth, Ltot_thisMonth, Dtot_thisMonth, Mtot_thisMonth, Ltot_All, Dtot_All, Mtot_All):
+def returned_data (currentMonth, CSVrows, Ltot_thisMonth, Dtot_thisMonth, Mtot_thisMonth, Ltot_All, Dtot_All, Mtot_All):
     __lineoffset__ = -4
     loop = ForLoop()
     self = TemplateResult(); extend_ = self.extend
@@ -85,6 +88,64 @@ def returned_data (CSVrows, currentMonth, Ltot_thisMonth, Dtot_thisMonth, Mtot_t
 
 returned_data = CompiledTemplate(returned_data, 'templates/returned_data.html')
 join_ = returned_data._join; escape_ = returned_data._escape
+
+# coding: utf-8
+def returned_data_bootstrap (currentMonth, CSVrows, Ltot_thisMonth, Dtot_thisMonth, Mtot_thisMonth, Ltot_All, Dtot_All, Mtot_All):
+    __lineoffset__ = -4
+    loop = ForLoop()
+    self = TemplateResult(); extend_ = self.extend
+    extend_([u'\n'])
+    extend_([u'<!DOCTYPE html>\n'])
+    extend_([u'<html lang="en">\n'])
+    extend_([u'        <head>\n'])
+    extend_([u'        <meta charset="utf-8">\n'])
+    extend_([u'        <meta name="viewport" content="width=device-width, initial-scale=1.0">\n'])
+    extend_([u'        <meta name="description" content="">\n'])
+    extend_([u'        <meta name="author" content="">\n'])
+    extend_([u'        <link rel="shortcut icon" href="../static/ico/favicon.png">\n'])
+    extend_([u'                            \n'])
+    extend_([u'        <title>myFood2.0 - tabulated</title>\n'])
+    extend_([u'                            \n'])
+    extend_([u'        <!-- Bootstrap core CSS -->\n'])
+    extend_([u'        <link href="../static/css/bootstrap.css" rel="stylesheet">\n'])
+    extend_([u'                                    \n'])
+    extend_([u'        <style type="text/css">\n'])
+    extend_([u'                .table-example{\n'])
+    extend_([u'                    margin: 20px;\n'])
+    extend_([u'                }\n'])
+    extend_([u'        </style>\n'])
+    extend_([u'        </head>\n'])
+    extend_([u'\n'])
+    extend_([u'        <body>\n'])
+    extend_([u'        <div class="table-example">\n'])
+    extend_([u'                <table class="table table-striped">\n'])
+    extend_([u'                        <thead>\n'])
+    extend_([u'                                <tr>\n'])
+    extend_([u'                                <th>Date</th>\n'])
+    extend_([u'                                <th>Lunch</th>\n'])
+    extend_([u'                                <th>Dinner</th>\n'])
+    extend_([u'                                <th>Miscellaneous</th>\n'])
+    extend_([u'                                </tr>\n'])
+    extend_([u'                        </thead>\n'])
+    extend_([u'                        <tbody>\n'])
+    for row in loop.setup(CSVrows):
+        extend_(['                        ', u'    <tr>\n'])
+        extend_(['                        ', u'    <td>', escape_(row[0], True), u'</td>\n'])
+        extend_(['                        ', u'    <td>', escape_(row[1], True), u'</td>\n'])
+        extend_(['                        ', u'    <td>', escape_(row[2], True), u'</td>\n'])
+        extend_(['                        ', u'    <td>', escape_(row[3], True), u'</td>\n'])
+        extend_(['                        ', u'    </tr>\n'])
+    extend_([u'                        </tbody>\n'])
+    extend_([u'                </table>\n'])
+    extend_([u'        </div>\n'])
+    extend_([u'    </body>\n'])
+    extend_([u'    \n'])
+    extend_([u'</html>\n'])
+
+    return self
+
+returned_data_bootstrap = CompiledTemplate(returned_data_bootstrap, 'templates/returned_data_bootstrap.html')
+join_ = returned_data_bootstrap._join; escape_ = returned_data_bootstrap._escape
 
 # coding: utf-8
 def tutorial (name):
