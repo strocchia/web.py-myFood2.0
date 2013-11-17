ed0b75c31e121d14d818e6d23257865f459fe1eb
Scott Trocchia <scotttrocchia@gmail.com>
2013-11-17 13:43:57 -0500

    11-17-13: successfully uploaded GAE site as myfood2dot0.appspot.com

--- code.py
+++ code.py
@@ -9,7 +9,8 @@ from collections import defaultdict
 
 urls = (
 	"/", "index",
-	"/getcsv", "getcsv"
+	"/getcsv", "getcsv",
+	"/nothingInCSV", "nothingInCSV"
 )
 
 render = web.template.render('templates/')
@@ -26,9 +27,7 @@ foodForm = form.Form(
 	form.Textbox('Clear? (y/n)')
 )
 
-#session = web.session.Session(app, web.session.DiskStore('sessions'))
-#myUserDict = web.session.Session(app, web.session.DiskStore('userdicts'))
-#user_store = web.session.Session(app, web.session.DiskStore('user_store'))
+#users_store = web.session.Session(app, web.session.DiskStore('users_store'), initializer={'double_dict': defaultdict(dict)})
 
 double_dict = defaultdict(dict)
 
@@ -44,6 +43,7 @@ class index:
 			clearMessage = ""
 
 			if form['Clear? (y/n)'].value == 'y':
+				#users_store['double_dict'].clear()
 				double_dict.clear()
 				clearMessage = "Your personal meal expense database is cleared!"
 				return render.init_form(form, clearMessage)
@@ -61,7 +61,7 @@ class index:
 				
 				joined_date = ' '.join([chosen_month, form['Day'].value, form['Year'].value])	
 
-				print 'DD b4: %s' % double_dict
+				print 'DD b4: %s' % users_store['double_dict']
 
 				if not user in double_dict:
 					### create the nested dict fields
@@ -105,34 +105,42 @@ class index:
 	
 				print 'CSVrows: %s' % double_dict[user]['CSVrows']
 				
-				return render.returned_data(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
-				#return render.returned_data_bootstrap(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
+				#return render.returned_data(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
+				return render.returned_data_bootstrap(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
 
 	def GET(self):
 		form = foodForm()
 		return render.init_form(form, "")
 
 
-""" Render and generate a CSV file, which includes all user input information up to this point in time """
+''' Render and generate a CSV file, which includes all user input information up to this point in time '''
 class getcsv:
 
 	def GET(self):
 
 		global user
-		if user:
-			print "personalized CSVrows: %s" % double_dict[user]['CSVrows']
 
-		#if double_dict[user]['CSVrows']:
-		csv_file = StringIO()
-		csv_writer = csv.writer(csv_file)
-		csv_writer.writerow(['Date', 'Lunch', 'Dinner', 'Miscellaneous'])
-		for i in double_dict[user]['CSVrows']:
-			csv_writer.writerow(i)
+		if len(double_dict) == 0:
+ 			raise web.seeother('/nothingInCSV')
+		else:
+			csv_file = StringIO()
+			csv_writer = csv.writer(csv_file)
+			csv_writer.writerow(['Date', 'Lunch', 'Dinner', 'Miscellaneous'])
+			for i in double_dict[user]['CSVrows']:
+				csv_writer.writerow(i)
 
-		currentDate = datetime.datetime.today().strftime('%m-%d-%Y')
-		web.header('Content-Type', 'text/csv')
-		web.header('Content-disposition', 'attachment; filename=MYFOOD_' + user.upper() + '_' + currentDate + '.csv')			
-		return csv_file.getvalue()
+			currentDate = datetime.datetime.today().strftime('%m-%d-%Y')
+			web.header('Content-Type', 'text/csv')
+			web.header('Content-disposition', 'attachment; filename=MYFOOD_' + user.upper() + '_' + currentDate + '.csv')			
+			return csv_file.getvalue()
+
+
+class nothingInCSV:
+
+	def GET(self):
+
+		nothingMessage = "You have not entered any data into your personal CSV file!"
+		return render.nothing_to_show(nothingMessage)
 
 
 app = app.gaerun()
