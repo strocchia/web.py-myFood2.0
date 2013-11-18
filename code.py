c77870dfdc22fdb9ab13801b55f0f8cb654198cb
Scott Trocchia <scotttrocchia@gmail.com>
2013-11-17 22:26:10 -0500

    everything has Twitter Bootstrap interfaces now.

--- code.py
+++ code.py
@@ -17,16 +17,16 @@ render = web.template.render('templates/')
 
 app = web.application(urls, globals())
 
-foodForm = form.Form(
-	form.Textbox('User name', web.form.notnull),
-	form.Dropdown('Which meal?', ['Lunch', 'Dinner', 'Miscellaneous']),
-	form.Dropdown('Month', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']),
-	form.Dropdown('Day', ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']),
-	form.Dropdown('Year', ['2013', '2014', '2015', '2016']),
-        form.Textbox('Lunch, Dinner, or Miscellaneous $'),
-	form.Textbox('Clear? (y/n)'),
-	form.Button('Submit')
-)
+#foodForm = form.Form(
+#	form.Textbox('User name', web.form.notnull),
+#	form.Dropdown('Which meal?', ['Lunch', 'Dinner', 'Miscellaneous']),
+#	form.Dropdown('Month', ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']),
+#	form.Dropdown('Day', ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31']),
+#	form.Dropdown('Year', ['2013', '2014', '2015', '2016']),
+#       form.Textbox('Lunch, Dinner, or Miscellaneous $'),
+#	form.Textbox('Clear? (y/n)'),
+#	form.Button('Submit')
+#)
 
 #users_store = web.session.Session(app, web.session.DiskStore('users_store'), initializer={'double_dict': defaultdict(dict)})
 
@@ -35,83 +35,79 @@ double_dict = defaultdict(dict)
 class index:
 
 	def POST(self):
-		form = foodForm()
 		
-		if not form.validates():
-			return render.init_form(form, "")
+		allFormInputs = web.input()
+
+		global user
+		clearMessage = ""
+
+		if allFormInputs.Clear == 'y':
+			double_dict.clear()
+			clearMessage = "Your personal meal expense database is cleared!"
+			return render.init_form_bootstrap(clearMessage)	
+
 		else:
-			global user
-			clearMessage = ""
+		### saving convenient values to make it easier to reference them in future parts of the code
+			
+			user = allFormInputs.Username
+			mealType = allFormInputs.Whichmeal
+			chosen_month = allFormInputs.Month
+			LDM = allFormInputs.LDM
+			LDM = float(LDM)
 
-			if form['Clear? (y/n)'].value == 'y':
-				#users_store['double_dict'].clear()
-				double_dict.clear()
-				clearMessage = "Your personal meal expense database is cleared!"
-				return render.init_form(form, clearMessage)
-	
+			currentMonth = datetime.datetime.now().month
+			monthDict={1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
+			currentMonth = monthDict[currentMonth]				
+			joined_date = ' '.join([chosen_month, allFormInputs.Day, allFormInputs.Year])
+
+			print 'DD b4: %s' % double_dict
+
+			if not user in double_dict:
+				### create the nested dict fields
+				double_dict[user]['dateList'] = []
+				double_dict[user]['lunchList'] = []
+				double_dict[user]['dinnerList'] = []
+				double_dict[user]['miscList'] = []
+				double_dict[user]['CSVrows'] = []
+				double_dict[user]['Ltot_thisMonth'] = 0.00
+				double_dict[user]['Dtot_thisMonth'] = 0.00
+				double_dict[user]['Mtot_thisMonth'] = 0.00
+				double_dict[user]['Ltot_All'] = 0.00
+				double_dict[user]['Dtot_All'] = 0.00
+				double_dict[user]['Mtot_All'] = 0.00
 			else:
-				### saving convenient values to make it easier to reference them in future parts of the code
-				user = form['User name'].value
-				mealType= form['Which meal?'].value
-				chosen_month  = form['Month'].value
-				LDM = float(form['Lunch, Dinner, or Miscellaneous $'].value)
-
-				currentMonth = datetime.datetime.now().month
-				monthDict={1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
-				currentMonth = monthDict[currentMonth]
+				print "User already exists!"
+			
+			### add to those fields
+			if not joined_date in double_dict[user]['dateList']:
+				double_dict[user]['dateList'].append(joined_date)
 				
-				joined_date = ' '.join([chosen_month, form['Day'].value, form['Year'].value])	
-
-				print 'DD b4: %s' % double_dict
-
-				if not user in double_dict:
-					### create the nested dict fields
-					double_dict[user]['dateList'] = []
-					double_dict[user]['lunchList'] = []
-					double_dict[user]['dinnerList'] = []
-					double_dict[user]['miscList'] = []
-					double_dict[user]['CSVrows'] = []
-					double_dict[user]['Ltot_thisMonth'] = 0.00
-					double_dict[user]['Dtot_thisMonth'] = 0.00
-					double_dict[user]['Mtot_thisMonth'] = 0.00
-					double_dict[user]['Ltot_All'] = 0.00
-					double_dict[user]['Dtot_All'] = 0.00
-					double_dict[user]['Mtot_All'] = 0.00
-				else:
-					print "User already exists!"
+			if mealType == "Lunch":
+				double_dict[user]['lunchList'].append(LDM)
+				double_dict[user]['Ltot_All'] += LDM
+				if chosen_month == currentMonth:
+					double_dict[user]['Ltot_thisMonth'] += LDM
+			elif mealType == "Dinner":
+				double_dict[user]['dinnerList'].append(LDM)
+				double_dict[user]['Dtot_All'] += LDM
+				if chosen_month == currentMonth:
+					double_dict[user]['Dtot_thisMonth'] += LDM
+			else:
+				double_dict[user]['miscList'].append(LDM)
+				double_dict[user]['Mtot_All'] += LDM
+				if chosen_month == currentMonth:
+					double_dict[user]['Mtot_thisMonth'] += LDM
 				
-				### add to those fields
-				if not joined_date in double_dict[user]['dateList']:
-					double_dict[user]['dateList'].append(joined_date)
-				
-				if mealType == "Lunch":
-					double_dict[user]['lunchList'].append(LDM)
-					double_dict[user]['Ltot_All'] += LDM
-					if chosen_month == currentMonth:
-						double_dict[user]['Ltot_thisMonth'] += LDM
-				elif mealType == "Dinner":
-					double_dict[user]['dinnerList'].append(LDM)
-					double_dict[user]['Dtot_All'] += LDM
-					if chosen_month == currentMonth:
-						double_dict[user]['Dtot_thisMonth'] += LDM
-				else:
-					double_dict[user]['miscList'].append(LDM)
-					double_dict[user]['Mtot_All'] += LDM
-					if chosen_month == currentMonth:
-						double_dict[user]['Mtot_thisMonth'] += LDM
-				
-				print 'DD after: %s' % double_dict
+			print 'DD after: %s' % double_dict
 
-				double_dict[user]['CSVrows'] = map(None, double_dict[user]['dateList'], double_dict[user]['lunchList'], double_dict[user]['dinnerList'], double_dict[user]['miscList'])
+			double_dict[user]['CSVrows'] = map(None, double_dict[user]['dateList'], double_dict[user]['lunchList'], double_dict[user]['dinnerList'], double_dict[user]['miscList'])
 	
-				print 'CSVrows: %s' % double_dict[user]['CSVrows']
+			print 'CSVrows: %s' % double_dict[user]['CSVrows']
 				
-				#return render.returned_data(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
-				return render.returned_data_bootstrap(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
+			return render.returned_data_bootstrap(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
 
 	def GET(self):
-		form = foodForm()
-		return render.init_form(form, "")
+		return render.init_form_bootstrap("")
 
 
 ### --> Render and generate a CSV file, which includes all user input information up to this point in time
