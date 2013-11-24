072ce752de351857e6e129ff1b641bdf0d26223b
Scott Trocchia <scotttrocchia@gmail.com>
2013-11-24 13:23:55 -0500

    using dropdown calendar instead of three separate date menus, for convenience

--- code.py
+++ code.py
@@ -29,17 +29,25 @@ class index:
 
 		global user, clearFlag, undoFlag
 		clearFlag = 0
-		clearMessage = ""
-
+		mealType = ""
+		
 		user = allFormInputs.Username
 		mealType = allFormInputs.Whichmeal
-		chosen_month = allFormInputs.Month	
+		
+		chosen_date = allFormInputs.MDY
+		chosen_date = chosen_date.split('/')
+		Month = chosen_date[0]
+		Day = chosen_date[1]
+		Year = chosen_date[2]		
+
 		currentMonth = datetime.datetime.now().month
 		monthDict={1:'January', 2:'February', 3:'March', 4:'April', 5:'May', 6:'June', 7:'July', 8:'August', 9:'September', 10:'October', 11:'November', 12:'December'}
 		currentMonth = monthDict[currentMonth]
-
+		
 		print ""	
 		print "DD immediately after POST called: %s" % double_dict
+
+		print allFormInputs.buttonDo
 
 		if allFormInputs.buttonDo == "Clear your database":
 			print "In the clear"
@@ -58,29 +66,30 @@ class index:
 			double_dict[user]['Mtot_All'] = 0.00
 			clearMessage = "Your personal meal expense database is cleared!"
 			print "DD after clear: %s" % double_dict
-			return render.init_form_bootstrap(clearMessage)	
+			return render.init_form_bootstrap_jquery(clearFlag)
 
 		elif allFormInputs.buttonDo == "Undo last entry":
+			print "Undo phase"
 			if mealType == "Lunch":
-				print "Undo lunch phase"
+				print "undo lunch"
 				last_L_entry = float(double_dict[user]['Ltot_All'])
 				double_dict[user]['lunchList'] = double_dict[user]['lunchList'][:-1]
 				double_dict[user]['Ltot_All'] -= last_L_entry
-				if chosen_month == currentMonth:
+				if Month == currentMonth:
 					double_dict[user]['Ltot_thisMonth'] -= last_L_entry
 			if mealType == "Dinner":
-				print "undo dinner phase"
+				print "undo dinner"
 				last_D_entry = float(double_dict[user]['Dtot_All'])
 				double_dict[user]['dinnerList'] = double_dict[user]['dinnerList'][:-1]
 				double_dict[user]['Dtot_All'] -= last_D_entry
-				if chosen_month == currentMonth:
+				if Month == currentMonth:
 					double_dict[user]['Dtot_thisMonth'] -= last_D_entry
 			if mealType == "Miscellaneous":
-				print "undo misc phase"
+				print "undo misc"
 				last_M_entry = float(double_dict[user]['Mtot_All'])
 				double_dict[user]['miscList'] = double_dict[user]['miscList'][:-1]
 				double_dict[user]['Mtot_All'] -= last_M_entry
-				if chosen_month == currentMonth:
+				if Month == currentMonth:
 					double_dict[user]['Mtot_thisMonth'] -= last_M_entry
 
 			double_dict[user]['CSVrows'] = map(None, double_dict[user]['dateList'], double_dict[user]['lunchList'], double_dict[user]['dinnerList'], double_dict[user]['miscList'])
@@ -90,11 +99,12 @@ class index:
 	
 		else:
 		### saving convenient values to make it easier to reference them in future parts of the code
-
+			print "saving data phase"
 			LDM = allFormInputs.LDM
 			LDM = float(LDM)	
 			
-			joined_date = ' '.join([chosen_month, allFormInputs.Day, allFormInputs.Year])
+			#joined_date = ' '.join([chosen_month, allFormInputs.Day, allFormInputs.Year])
+			joined_date = ' '.join([Month, Day, Year])
 
 			print 'DD b4: %s' % double_dict
 
@@ -122,19 +132,21 @@ class index:
 			if mealType == "Lunch":
 				double_dict[user]['lunchList'].append(LDM)
 				double_dict[user]['Ltot_All'] += LDM
-				if chosen_month == currentMonth:
+				if Month == currentMonth:
 					double_dict[user]['Ltot_thisMonth'] += LDM
 			elif mealType == "Dinner":
 				double_dict[user]['dinnerList'].append(LDM)
 				double_dict[user]['Dtot_All'] += LDM
-				if chosen_month == currentMonth:
+				if Month == currentMonth:
 					double_dict[user]['Dtot_thisMonth'] += LDM	
-			else:
+			elif mealType == "Miscellaneous":
 				double_dict[user]['miscList'].append(LDM)
 				double_dict[user]['Mtot_All'] += LDM
-				if chosen_month == currentMonth:
+				if Month == currentMonth:
 					double_dict[user]['Mtot_thisMonth'] += LDM
-			
+			else:
+				print ""
+
 			print 'DD after: %s' % double_dict
 			
 			double_dict[user]['CSVrows'] = map(None, double_dict[user]['dateList'], double_dict[user]['lunchList'], double_dict[user]['dinnerList'], double_dict[user]['miscList'])
@@ -145,7 +157,7 @@ class index:
 			return render.returned_data_bootstrap(currentMonth, double_dict[user]['CSVrows'], double_dict[user]['Ltot_thisMonth'], double_dict[user]['Dtot_thisMonth'], double_dict[user]['Mtot_thisMonth'], double_dict[user]['Ltot_All'], double_dict[user]['Dtot_All'], double_dict[user]['Mtot_All'])
 
 	def GET(self):
-		return render.init_form_bootstrap("")
+		return render.init_form_bootstrap_jquery(0)
 
 
 ### --> Render and generate a CSV file, which includes all user input information up to this point in time
